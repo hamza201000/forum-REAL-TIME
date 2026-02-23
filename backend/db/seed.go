e0e19423e771bc9b848d15c91d6039f61aeed064
hamza201000 <hamzaghomari700@gmail.com>
2026-02-23 06:08:52 +0000

    create DataBase

--- backend/db/seed.go
+++ backend/db/seed.go
@@ -1,1 +1,29 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./social.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlFile, err := os.ReadFile("db/schema.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(string(sqlFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Database & tables created successfully")
+
+	return db
+}
--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"net/http"
 
+	"backend/db"
 	"backend/handlers"
 	"backend/routes"
 )
 
-
-
 func main() {
+	DB := db.InitDB()
+	defer DB.Close()
 	routes.RegisterRoutes()
 	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.ListenAndServe(":8080", nil)
